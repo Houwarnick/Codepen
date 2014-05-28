b80a8df3e1a0a9a9792f9c6cfa7c96e7e570ebc0
Houston <houwarnick@gmail.com>
2014-05-27 18:19:49 -0600

    working solution up to 5

--- script.js
+++ script.js
@@ -27,10 +27,61 @@ Reponse Objects:
 	('jerry', '@#!$%@')
 
 */
+function user(name, email, username, password) {
+	  this.name = name;
+	  this.email = email;
+	  this.username = username;	
+	  this.password = password;
+	  this.isLoggedIn = false;
+}
 
 
 $('document').ready(function() {
+	//login and signup forms hide/show code
 
-	//put code here
+	$('.login-form-btn').click(function() {
+	  $('.signup-form-btn').removeClass('active');
+	  $('.login-form-btn').addClass('active');
+	  $('.signup-form').css('display','none');
+	  $('.login-form').css('display','block');
+
+	});
+	$('.signup-form-btn').click(function() {
+	  $('.login-form-btn').removeClass('active');
+	  $('.signup-form-btn').addClass('active');
+	  $('.login-form').css('display','none');
+	  $('.signup-form').css('display','block');
+	  
+	});
+
+	//Login code 
+	$('.btn-login').click(function(){
+	  var person = new user('','',$('#login-username-field').val(), $('#login-password-field').val());
+	  $('.form-feedback').empty();
+	  if(codepen.api.login(person).success){
+	  	$('.form-feedback').html('Logged In!');
+	  	person.isLoggedIn = true;
+	  }
+	  else{
+	  	$('.form-feedback').html(codepen.api.login(person).error);
+	  }
+	});
+
+	//Signup code
+	$('.btn-signup').click(function(){
+      $('.form-feedback').empty();
+	  var person = new user ( $('#signup-name-field').val(),  $('#signup-email-field').val(),  $('#signup-username-field').val(),  $('#signup-password-field').val());
+	  if($('#signup-password-field').val() !== $('#signup-password-field2').val()){
+	  	$('.form-feedback').html('Passwords Do Not Match');
+	  }
+	  else{
+	    if(codepen.api.signup(person).success){
+	  	  $('.form-feedback').html('Signed Up!');
+	    }
+	    else{
+	  	  $('.form-feedback').html(codepen.api.signup(person).error);
+	    }
+	  }
+	});
 
 });
